aeb7c1e43ea47d7ed0dd98a5cc7352a92fcadc74
Animesh Kumar <animesh@complyindia.com>
2019-09-20 14:21:00 +0530

    The Vue Instance - Creating a Vue Instance, Data and Methods, Instance Lifecycle Hooks

--- js/custom_vue_scripts.js
+++ js/custom_vue_scripts.js
@@ -71,3 +71,90 @@ var app6 = new Vue({
       ]
     }
   })
+
+  // Creating a Vue Instance
+  var vm = new Vue({
+    // options
+  })
+
+  // Data and Methods
+  // Our data object
+var data = { a: 1 }
+
+// The object is added to a Vue instance
+var vm = new Vue({
+  el: '#app-8',
+  data: data
+})
+
+// Getting the property on the instance
+// returns the one from the original data
+vm.a == data.a // => true
+
+// Setting the property on the instance
+// also affects the original data
+vm.a = 2
+data.a // => 2
+
+// ... and vice-versa
+data.a = 3
+vm.a // => 3
+
+// Add a new property
+vm.b = 'hi' //Then changes to b will not trigger any view updates
+
+// set some initial value
+/**
+data: {
+  newTodoText: '',
+  visitCount: 0,
+  hideCompletedTodos: false,
+  todos: [],
+  error: null
+}
+ * */ 
+
+//  Use of Object.freeze(), which prevents existing properties from being changed
+
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+new Vue({
+  el: '#app-9',
+  data: obj
+})
+
+
+// In addition to data properties, Vue instances expose a number of useful instance properties and methods. These are prefixed with $ to differentiate them from user-defined properties
+var data = { a: 1 }
+var vm = new Vue({
+  el: '#app-10',
+  data: data
+})
+
+vm.$data === data // => true
+vm.$el === document.getElementById('app-10') // => true
+// alert(vm.$el === document.getElementById('app-10'))
+// $watch is an instance method
+vm.$watch('a', function (newValue, oldValue) {
+  // This callback will be called when `vm.a` changes
+  alert('triggered')
+})
+
+// Instance Lifecycle Hooks :-
+  // The created hook can be used to run code after an instance is created
+new Vue({
+  el: '#app-11',
+  data: {
+    a: 1
+  },
+  created: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.a)
+  }
+})
+// => "a is: 1"
+  // There are also other hooks which will be called at different stages of the instance’s lifecycle, such as mounted, updated, and destroyed. All lifecycle hooks are called with their this context pointing to the Vue instance invoking it
